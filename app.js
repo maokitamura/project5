f980b662eb02d718977c4e83b6fef69bfc8c7dcb
Mao <maokitty23k@gmail.com>
2021-08-12 23:59:29 -0400

    added media

---
+++ app.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.getElementById("burger-menu");
+const overlay = document.getElementById("menu");
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
